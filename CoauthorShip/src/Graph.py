4ed4fa57310bc1520b82b4223742f6765be91086
Tengyu Wang <tengyu-wang@uiowa.edu>
2013-03-25 23:37:16 -0500

    BaseLine for coauthorship network analysis.

---
+++ CoauthorShip/src/Graph.py
@@ -0,0 +1,53 @@
+'''
+Created on Mar 25, 2013
+
+@author: Tengyu
+'''
+import networkx as nx
+class Graph:
+    '''
+    The graph to be built from co-authorship
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.graph=nx.empty_graph()
+    
+    def buildGraph(self,fileName):
+        '''
+        Build a graph from input file.
+        '''
+        line=''
+        f = open(fileName,'r')
+        while True:
+            line=f.readline()[24:]
+            if not line:
+                break;
+            line=line[:line.find(',')]
+            if(line==''): #in case there is no author for a paper
+                continue
+            authors=line.split(' & ')
+            count=len(authors)
+            if(count==1):
+                self.graph.add_node(authors[0]) 
+            for i in range(0,count-1):
+                for j in range(i+1,count):
+                    self.graph.add_edge(authors[i],authors[j])
+        return
+    
+    def getNumNodes(self):
+        '''
+        Get the number of nodes in the graph
+        '''
+        return self.graph.number_of_nodes()
+    
+    def getNumEdges(self):
+        '''
+        Get the number of edges in the graph
+        '''
+        return self.graph.number_of_edges()
+            
+        
---
+++ CoauthorShip/src/Test.py
@@ -0,0 +1,38 @@
+'''
+Created on Mar 25, 2013
+
+@author: Tengyu
+'''
+import Graph
+
+if __name__ == '__main__':
+    pass
+
+def degreeAnalysis(G):
+    '''
+    Analyze the degree distribution
+    '''
+    pass
+
+def componentAnalysis(G):
+    '''
+    Analyze the connected component, question 1)
+    '''
+    pass
+
+def nodeDistance(G, root, k):
+    '''
+    Get the number of nodes at distance exactly k from the root, qusetion 2)
+    '''
+    pass
+
+def nodeDiatanceAnalysis(G):
+    '''
+    Analyze the node distance and solve question 3) and 4)
+    '''
+    pass
+
+G=Graph.Graph()
+G.buildGraph('inputFile/papers.lst')
+
+
