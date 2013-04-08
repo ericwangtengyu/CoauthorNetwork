0fcd41cdafa9cc6bdf7d42490ef7c0b7d0dc55c5
Tengyu Wang <tengyu-wang@uiowa.edu>
2013-04-07 21:01:37 -0500

    Baseline of Component Analysis.

--- CoauthorShip/src/Test.py
+++ CoauthorShip/src/Test.py
@@ -3,6 +3,7 @@ Created on Mar 25, 2013
 
 @author: Tengyu
 '''
+import networkx as nx
 import Graph
 
 if __name__ == '__main__':
@@ -18,8 +19,14 @@ def componentAnalysis(G):
     '''
     Analyze the connected component, question 1)
     '''
-    pass
-
+    print('--------Connected component analysis--------')
+    H=nx.connected_component_subgraphs(G)[0]
+    n1=H.number_of_nodes()
+    n=G.number_of_nodes()
+    print('Number of Nodes in the largest component: ',n1)
+    print('Number of Nodes in total: ',n)
+    print('The ratio is: ',float(n1)/n)
+    
 def nodeDistance(G, root, k):
     '''
     Get the number of nodes at distance exactly k from the root, qusetion 2)
@@ -34,5 +41,6 @@ def nodeDiatanceAnalysis(G):
 
 G=Graph.Graph()
 G.buildGraph('inputFile/papers.lst')
+componentAnalysis(G.graph)
 
 
