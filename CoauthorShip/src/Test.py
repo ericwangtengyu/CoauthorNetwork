50a68516beb6c5ef59bc9165a845cf752941cf61
Tengyu Wang <tengyu-wang@uiowa.edu>
2013-04-07 22:03:03 -0500

    Fix Merge issue.

--- CoauthorShip/src/Test.py
+++ CoauthorShip/src/Test.py
@@ -4,7 +4,10 @@ Created on Mar 25, 2013
 @author: Tengyu
 '''
 import networkx as nx
+<<<<<<< HEAD
 import numpy as np
+=======
+>>>>>>> refs/heads/ComponentAnalysis
 import matplotlib.pyplot as plt
 import Graph
 
@@ -39,8 +42,22 @@ def componentAnalysis(G):
     '''
     Analyze the connected component, question 1)
     '''
-    pass
-
+    print('--------Connected component analysis--------')
+    H=nx.connected_component_subgraphs(G)
+    n1=H[0].number_of_nodes()
+    n=G.number_of_nodes()
+    ratio=float(n1)/n
+    print('Number of Nodes in the largest component: ',n1)
+    print('Number of Nodes in total: ',n)
+    print('The ratio is: ',ratio)
+    print('The coauthorNetwork has a giant component')
+    labels = 'Others','Largest Component'
+    fracs = [1-ratio,ratio]
+    explode=(0.08, 0)
+    plt.title('Connected Component Analysis')
+    plt.pie(fracs, explode=explode, labels=labels,autopct='%1.1f%%', shadow=True, startangle=90)
+    plt.show()
+    
 def nodeDistance(G, root, k):
     '''
     Get the number of nodes at distance exactly k from the root, qusetion 2)
